cc468629df80f76599eb27b37ecf9df9f31c331d
Jarrett Bariel <jbariel@codewhispereronsulting.com>
2020-06-11 23:48:22 +0000

    basic multi-module that builds/runs

---
+++ consumer/src/main/java/com/jbariel/example/wss/consumer/Consumer.java
@@ -0,0 +1,15 @@
+package com.jbariel.example.wss.consumer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Consumer {
+
+    private final static Logger log = LoggerFactory.getLogger(Consumer.class);
+
+    public static void main(final String[] args) {
+        log.info("Hello World from Consumer");
+
+        System.exit(0);
+    }
+}
---
+++ publisher/src/main/java/com/jbariel/example/wss/publisher/Publisher.java
@@ -0,0 +1,15 @@
+package com.jbariel.example.wss.publisher;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Publisher {
+
+    private final static Logger log = LoggerFactory.getLogger(Publisher.class);
+
+    public static void main(final String[] args) {
+        log.info("Hello World from Publisher");
+
+        System.exit(0);
+    }
+}
